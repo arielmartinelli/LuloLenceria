0646d3ba28850f4788ed4f322dec6658a76de0f6
Ariel Martinelli <arielmartinelli2019@gmail.com>
2026-08-12 15:56:26 -0300

    update_exact_pink_silk_metaimg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
       {
         url: 'https://lulo-lenceria.vercel.app/metaimg.png',
         width: 1200,
-        height: 1200,
+        height: 630,
         alt: 'Lulo Lencería',
       },
     ],
